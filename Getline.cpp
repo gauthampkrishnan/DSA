523c0eb4bc560b2774fadf7d3d170343313e0cbe
gauthampkrishnan@ieee.org <gauthampkrishnan@ieee.org>
2021-10-29 17:38:14 -0400

    Getline in C++

--- Getline.cpp
+++ Getline.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     char sentence[1000];
     char para[1000];
-    cin.getline(para,10000,'.');
+    cin.getline(para,10000,'\n'); //stopping condition \n 
     cout<<para;
     return 0;
 }
