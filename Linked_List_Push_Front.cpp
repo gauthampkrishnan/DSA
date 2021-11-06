70bd5079b37fd49188e54bf606e77bef548c34e2
gauthampkrishnan@ieee.org <gauthampkrishnan@ieee.org>
2021-11-05 20:23:38 -0400

    LinkedList-1, C++

--- Linked_List_Push_Front.cpp
+++ Linked_List_Push_Front.cpp
@@ -1,36 +1,64 @@
 #include<iostream>
 using namespace std;
+class List;
 class Node{
     public:
-
     int data;
-    Node* next;
-};
-void printList(Node *n){
-    while(n!=NULL){
-        cout<<n->data<<"->";
-        n=n->next;
+    Node *next;
+    Node(int d):data(d), next(NULL){
     }
+     int getData(){
+        return data;
+    }
+    friend class List;
+};
 
-}
-
+class List{
+    Node * head;
+    Node * tail;
+    public:
+    List():head(NULL),tail(NULL){}
+    Node* begin(){
+        return head;
+    }
+   
+    void pushfront(int data){
+        if(head==NULL){
+            Node* n= new Node(data);
+            head =tail=n;
+            return;
+        }
+        else{
+            Node* n= new Node(data);
+            n->next=head;
+            head=n;
+        }
+    }
+    void pushback(int data){
+        if(head==NULL){
+             Node* n= new Node(data);
+              head =tail=n;
+        }
+        else{
+             Node* n= new Node(data);
+             tail->next=n;
+             tail=n;
+        }
+        
+    }
+    
+};
 
 int main(){
-    Node* head=NULL;
-    Node* second =NULL;
-    Node* third=NULL;
-
-    head=new Node();
-    second=new Node(); 
-    third=new Node();
-
-    head->data=1;
-    head->next=second;
-    second->data=2;
-    second->next=third;
-    third->data=3;
-    third->next=NULL; 
-
-    printList(head);
+    List l;
+    l.pushfront(1);
+    l.pushback(2);
+      l.pushback(3);
+      l.pushfront(7);
+    Node* head=l.begin();
+    while(head!=NULL){
+        cout<<head->getData()<<"->";
+        head=head->next;
+    }
     return 0;
 }
